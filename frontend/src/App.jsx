3b0319fc47126093b5dc334229ccfe0ddd3252fd
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2023-12-07 03:56:40 +0800

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>Selamat Datang di Website Ilhamrhmddni</div>
+    </>
+  )
+}
+
+export default App
